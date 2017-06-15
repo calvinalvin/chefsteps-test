f35e0d57a40ae6663d38425b936677debf99f647
calvinalvin <calvinoh@gmail.com>
2017-06-14 17:46:20 -0700

    Update deducer to use a for loop against array, update test case to include existence check of deduced against originals

--- test/test.js
+++ test/test.js
@@ -13,11 +13,22 @@ describe('EmailGenerator', function() {
 
 describe('Deduper', function() {
   describe('#clean()', function() {
-    it('shouuld dedupe duplicate emails', function() {
+    it('should dedupe duplicate emails', function() {
       let emails = EmailGenerator.generateWithDupes(10, 0.5);
       let deduped = deduper.clean(emails);
       // the length of the deduped emails should be 5 because that's 50% of the dupes
       assert.equal(deduped.length, 5);
+
+      // 2nd part of test
+      // store a check for all deduped emails and then iterate originals to make sure deduped contain all of originals
+      let exists = {};
+      deduped.forEach((d) => {
+        exists[d] = true;
+      });
+
+      emails.forEach((e) => {
+        assert.ok(exists[e]);
+      });
     });
   })
 });
